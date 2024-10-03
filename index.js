4b884851cb56e01bc6ae2444fe3a38ba12508865
CauaSF <cazinhoferreira@gmail.com>
2024-10-02 21:48:12 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+var atual =2024;
+var nascimento=2008;
+
+console.log(atual - nascimento);
+
+// soma +
+// subtração -
+// multiplicação *
+// divisão /
+
+
+
+
+
+
+
+
+
+
+// const nome = "Cauã";
+// const sobrenome = 'Santos';
+// const texto = `Colocar o texto`;
+// const nomeCompleto = nome + ' ' + sobrenome
+// const idade = 16
+// const frase = "O nome da pessoa é: " + nomeCompleto + ". E sua idade é de " + idade + " anos"
+// const frase2 = `O nome da pessoa é ${nomeCompleto}. E sua idade é ${idade} anos`;
+
+// console.log(frase2)
+
+// node index.js
+
+// cls - clear screen, só é valido no windows -CMD/Powershell
+// clear - # Linux / MacOS - Bash
+//console.log() serve para chamar as funções 
+
+// Nunca iniciar o nome com um número | var 1nome; (errado)
+// Nunca acentuar a variávei | var profissão; (errado) | correto: var profissao;
+// O único caracter especial aceito é o underline/underscore _ | var _nome; | var nome_profissao;
+// Nunca separar por espaço, utilizar o underlin/underscore | 
+// errado: var primeiro nome;  | correto: var primeiro_nome;
+// snake_case: separados por underline/underscore.
+// PascalCase: palavras iniciadas em letras maiúsculas.
+// camelCase: palavras separadas iniciadas em letras maiúsculas.
